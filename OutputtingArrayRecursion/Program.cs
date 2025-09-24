c55681f94b445b2eb0558f6a00049e1aece34a62
AwallenceCode <awallencecode@gmail.com>
2025-09-24 14:29:19 +0300

    A method for generating an array using recursion was written

--- OutputtingArrayRecursion/Program.cs
+++ OutputtingArrayRecursion/Program.cs
@@ -1,3 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+class MyClass
+{
+    static void Generaterray(int[] programmArry, Random rnd, int iterator)
+    {
+        if (iterator >= programmArry.Length) return;
+        
+        programmArry[iterator] = rnd.Next(-11, 11);
+        
+        Generaterray(programmArry, rnd, iterator + 1);
+    }
 
-Console.WriteLine("Hello, World!");
+    static void PrintArray(int[] programmArray, int iterator)
+    {
+        if (iterator >= programmArray.Length) return;
+    }
+    static void Main()
+    {
+        Random rnd = new Random();
+        int iterator = 0;
+        
+        const byte arraySize = 15;
+        int[] programmArray = new int [arraySize];
+
+        Generaterray(programmArray, rnd, iterator);
+        PrintArray(programmArray, iterator);
+
+    }
+}
